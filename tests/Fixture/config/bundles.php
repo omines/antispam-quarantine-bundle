3b7ef8737f4a6158be801b17da1c24fe7fa25a4f
Niels Keurentjes <niels.keurentjes@omines.com>
2025-01-20 22:26:44 +0100

    Set up core test cases

---
+++ tests/Fixture/config/bundles.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Omines\AntiSpamBundle\AntiSpamBundle::class => ['all' => true],
+    Omines\AntiSpamQuarantineBundle\AntiSpamQuarantineBundle::class => ['all' => true],
+];
